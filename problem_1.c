3a7d8fdd9d99e22405743ef5d885cfab7c6deafd
srayo16 <srayosikder@gmail.com>
2023-04-04 06:06:07 +0600

    first commit

---
+++ problem_1.c
@@ -0,0 +1,13 @@
+// You need to print the lines given below in the same format as it is -
+
+// “Recently I heard that you’ve achieved 95% marks in your exam.
+// This is brilliant!
+// I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life.”
+
+#include <stdio.h>
+
+int main()
+{
+
+    return 0;
+}
